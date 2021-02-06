877eca7ce0fdab558a93123b94506591bf549bb8
Nipun Khanna <2Nipun@gmail.com>
2021-02-06 14:26:59 -0800

    Multiple files & AWS changes
    . Creates for 2 files now (needs to be automated more)
    . Upload to AWS is public now

--- task.py
+++ task.py
@@ -43,17 +43,20 @@ color_orange = (254, 80, 0)
 # For Youtube Thumbnail - CovidFeedDaily 
 today = date.today()
 date_today = today.strftime("%B %d, %Y")
-input_file = 'input/youtubeCovidDaily.png'
-output_filename = 'ytCovidDaily-'+ today.strftime("%b-%d-%Y")
+input_file1 = 'YTDailyMobiSource.png'
+input_file2 = 'YTDailyTVSource.png'
+# output_filename = 'ytCovidDaily-'+ today.strftime("%b-%d-%Y")
 
 ## Main function to create
 def create_frame(
-	text_entry = 'January 23, 2021',
-	input_file = 'input/youtubeCovidDaily.png',
-	file_type = 'file',
-	outputfilename='output_alpha',
+	text_entry,
+	input_file,
+	file_type,
+	outputfilename
 	):
-
+	
+	input_file = 'input/'+input_file
+	print ('Input file path:', input_file)
 	#Loading files or a stream, older code will be files mostly here 
 	if file_type == 'stream':
 		input_image = Image.open(io.BytesIO(input_file)) #for handling data in ioStream <bytes> format
@@ -77,11 +80,11 @@ def create_frame(
 # Dumping to AWS 
 def dumpToS3(file_name, bucket='amcreativebucket', object_name=None):
     # If S3 object_name was not specified, use file_name
-    url_s3 = f"https://{bucket}.s3.{aws_region}.amazonaws.com/{file_name}" #Manually creating structure
+    url_s3 = f"https://{bucket}.s3-us-west-2.amazonaws.com/{file_name}" #Manually creating structure
     object_name = file_name
     file_path = './output/'+file_name
     try:
-        response = s3.upload_file(file_path, bucket, object_name)
+        response = s3.upload_file(file_path, bucket, object_name, ExtraArgs={'ACL':'public-read'}) #Enables public read of file
         print ("✅ Upload to AWS Success..")
         return url_s3
     except ClientError as e:
@@ -89,17 +92,18 @@ def dumpToS3(file_name, bucket='amcreativebucket', object_name=None):
         return ('🚫Error uploading to S3: '+str(e))
 
 # Dumping to service Dump after all is run
-def dumpToAirtable(inputURL):
+def dumpToAirtable(inputURL, input_file):
 	time_pulled = str(datetime.now())
 	amService = 'amCreative_Thumbnail'
 	data_output = str(inputURL)
-	UUID = 'ytThumb-'+str(shortuuid.uuid()) #to create unique file name
+	UUID = 'ytThumb-'+input_file+'-'+str(shortuuid.uuid()) #to create unique file name
 	fields = {'UUID':UUID, 'time_pulled':time_pulled, 'data_output': data_output, 'amService':amService }
 	airtable_dump.insert(fields)
 	print ("✅ Upload to Airtable Success.")
 
 # Main loop that does all actions separately
-def runloop():
+def runloop(input_file):
+	output_filename = str(input_file)+ today.strftime("%b-%d-%Y")
 	file_created = create_frame(
 		text_entry = date_today,
 		input_file = input_file,
@@ -107,9 +111,10 @@ def runloop():
 		outputfilename=output_filename,
 		)
 	url_uploaded = dumpToS3(file_created)
-	dumpToAirtable(url_uploaded)
+	dumpToAirtable(url_uploaded, input_file)
 
 # Running service
-runloop()
+runloop(input_file1)
+runloop(input_file2)
 
 
